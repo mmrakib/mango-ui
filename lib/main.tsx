c997c114679f0b155835ef42f391ad2a28cfad2e
Mayaz Rakib <mmrakib@Mayazs-MacBook-Air.local>
2023-12-12 21:08:33 +1100

    Added inverted button

--- lib/main.tsx
+++ lib/main.tsx
@@ -1,10 +1,12 @@
 // import Component from './path/to/component.tsx'
 // export { Component, ... }
 
+import InvertedButton from './Buttons/InvertedButton.tsx'
 import FloatingButton from './Buttons/FloatingButton.tsx'
 import GithubButton from './Buttons/GithubButton.tsx'
 
 export {
+    InvertedButton,
     FloatingButton,
     GithubButton
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Canvas from './Canvas.tsx'
 import { createGlobalStyle } from 'styled-components'
 
-import { FloatingButton } from '../lib/main.tsx'
+import { InvertedButton } from '../lib/main.tsx'
 
 const GlobalStyle = createGlobalStyle`
   body, html {
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyle />
     <Canvas>
-      <FloatingButton>This is a button</FloatingButton>
+      <InvertedButton>This is a button</InvertedButton>
     </Canvas>
   </React.StrictMode>,
 )
